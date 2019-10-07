512b2f8e3945c251cc91a2b3e31dfc64f695d02c
abhimanyu <abhimanyukrsingh00@gmail.com>
2019-10-08 00:27:26 +0530

    Update myapi.py

--- myapi.py
+++ myapi.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_restful import Resource, Api
 import redis, json
+import operator
 
 r = redis.Redis()
 app = Flask(__name__)
 api = Api(app)
 
 
-class show_transactions(Resource):
+class showTransactions(Resource):
     def get(self):
         dic = {}
         li = [x for x in r.lrange("transactions", 0, -1)]
@@ -18,27 +19,41 @@ class show_transactions(Resource):
         return dic
 
 
-class transactions_count_per_minute(Resource):
-    def get(self):
+class transactionsCount(Resource):
+    def get(self, min_value=None):
         try:
             keys = r.keys('r*')
-            if b'transactions' in keys: keys.remove(b'transactions')
             keys.sort()
             dic = {}
             for key in keys:
                 dic[key.decode("utf-8")[1:]] = r.get(key).decode("utf-8")
-            return dic
+
+            if min_value is None:
+                return dic
+
+            if min_value in dic:
+                return dic[min_value]
 
         except Exception as e:
             return {'Error': e}
 
 
-class high_value_addr(Resource):
+class highValueAddr(Resource):
     def get(self):
-        return {'hello3': 'world'}
+        try:
+            keys = r.keys('v*')
+            dic = {}
+            for ikey in keys:
+                dic[ikey.decode("utf-8")[1:]] = int(r.get(ikey).decode("utf-8"))
+            sorted_x = sorted(dic.items(), key=operator.itemgetter(1), reverse=True)
+            # returning top 100 only for readability
+            return sorted_x[:100]
+
+        except Exception as e:
+            return {'Error': e}
 
 
-api.add_resource(show_transactions, '/show_transactions', '/')
-api.add_resource(transactions_count_per_minute, '/transactions_count_per_minute/')
-api.add_resource(high_value_addr, '/high_value_addr')
+api.add_resource(showTransactions, '/show_transactions')
+api.add_resource(transactionsCount, '/transactions_count_per_minute/<min_value>', '/transactions_count_per_minute/')
+api.add_resource(highValueAddr, '/high_value_addr')
 app.run(debug=True)
